839a94c9e746620c7b6c8cf98bbcad0217fb674b
Vincent Toon <vincenttoon22@gmail.com>
2024-01-16 23:21:37 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, createContext } from "react";
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./Words";
+
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+
+  return (
+    <div className="App">
+      <nav>
+        <h1>vwortle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{ board, setBoard, currAttempt, setCurrAttempt }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { boardDefault } from '../Words'
+import Letter from './Letter';
+
+const Board = () => {
+
+  return (
+    <div className='board'>
+        <div className='row'>
+            <div className='row'><Letter letterPos={0} attemptVal={0}/></div>
+            <div className='row'><Letter letterPos={1} attemptVal={0}/></div>
+            <div className='row'><Letter letterPos={2} attemptVal={0}/></div>
+            <div className='row'><Letter letterPos={3} attemptVal={0}/></div>
+            <div className='row'><Letter letterPos={4} attemptVal={0}/></div>
+            <div className='row'><Letter letterPos={5} attemptVal={0}/></div>
+        </div>
+        <div className='row'>
+            <div className='row'><Letter letterPos={0} attemptVal={1}/></div>
+            <div className='row'><Letter letterPos={1} attemptVal={1}/></div>
+            <div className='row'><Letter letterPos={2} attemptVal={1}/></div>
+            <div className='row'><Letter letterPos={3} attemptVal={1}/></div>
+            <div className='row'><Letter letterPos={4} attemptVal={1}/></div>
+            <div className='row'><Letter letterPos={5} attemptVal={1}/></div>
+        </div>
+        <div className='row'>
+            <div className='row'><Letter letterPos={0} attemptVal={2}/></div>
+            <div className='row'><Letter letterPos={1} attemptVal={2}/></div>
+            <div className='row'><Letter letterPos={2} attemptVal={2}/></div>
+            <div className='row'><Letter letterPos={3} attemptVal={2}/></div>
+            <div className='row'><Letter letterPos={4} attemptVal={2}/></div>
+            <div className='row'><Letter letterPos={5} attemptVal={2}/></div>
+        </div>
+        <div className='row'>
+            <div className='row'><Letter letterPos={0} attemptVal={3}/></div>
+            <div className='row'><Letter letterPos={1} attemptVal={3}/></div>
+            <div className='row'><Letter letterPos={2} attemptVal={3}/></div>
+            <div className='row'><Letter letterPos={3} attemptVal={3}/></div>
+            <div className='row'><Letter letterPos={4} attemptVal={3}/></div>
+            <div className='row'><Letter letterPos={5} attemptVal={3}/></div>
+        </div>
+        <div className='row'>
+            <div className='row'><Letter letterPos={0} attemptVal={4}/></div>
+            <div className='row'><Letter letterPos={1} attemptVal={4}/></div>
+            <div className='row'><Letter letterPos={2} attemptVal={4}/></div>
+            <div className='row'><Letter letterPos={3} attemptVal={4}/></div>
+            <div className='row'><Letter letterPos={4} attemptVal={4}/></div>
+            <div className='row'><Letter letterPos={5} attemptVal={4}/></div>
+        </div>
+        <div className='row'>
+            <div className='row'><Letter letterPos={0} attemptVal={5}/></div>
+            <div className='row'><Letter letterPos={1} attemptVal={5}/></div>
+            <div className='row'><Letter letterPos={2} attemptVal={5}/></div>
+            <div className='row'><Letter letterPos={3} attemptVal={5}/></div>
+            <div className='row'><Letter letterPos={4} attemptVal={5}/></div>
+            <div className='row'><Letter letterPos={5} attemptVal={5}/></div>
+        </div>
+        { " " }
+
+    </div>
+  )
+};
+
+export default Board
