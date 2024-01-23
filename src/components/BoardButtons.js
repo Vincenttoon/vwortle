72cc58db191bf4e98b20890fc7c8924db6ea7fc1
Vincent Toon <vincenttoon22@gmail.com>
2024-01-23 12:48:16 -0600

    cpu size correct

--- src/components/BoardButtons.js
+++ src/components/BoardButtons.js
@@ -4,19 +4,19 @@ const BoardButtons = () => {
   return (
     <div className="choices-div">
       <a href="/FourBoard">
-        <button className="choice-button">4 Letters</button>
+        <button className="choice-button choice-4">4 Letters</button>
       </a>
       <a href="/FiveBoard">
-        <button className="choice-button">5 Letters</button>
+        <button className="choice-button choice-5">5 Letters</button>
       </a>
       <a href="/">
-        <button className="choice-button">6 Letters</button>
+        <button className="choice-button choice-6">6 Letters</button>
       </a>
       <a href="/SevenBoard">
-        <button className="choice-button">7 Letters</button>
+        <button className="choice-button choice-7">7 Letters</button>
       </a>
       <a href="/EightBoard">
-        <button className="choice-button">8 Letters</button>
+        <button className="choice-button choice-8">8 Letters</button>
       </a>
     </div>
   );
